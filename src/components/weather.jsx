aab285e19d8730e0313b5fcfdda6b3001ea6f4a4
AY-Dannex <ayomidedaniel1212@gmail.com>
2025-08-08 00:04:16 +0100

    Added suggestions to search and some other minute changes

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 import SkeletonLoader from "./SkeletonLoader";
 import search from '../assets/search.png';
-import clearSky from '../assets/clear.png'
-import clouds from '../assets/clouds.png'
-import haze from '../assets/haze.png'
-import overcast from '../assets/overcast-cloud.png'
-import broken from '../assets/broken-clouds.png'
 import location from '../assets/location.png'
 import humidityIcon from '../assets/humidity.png'
 import windIcon from '../assets/wind.png'
 import pressureIcon from '../assets/pressure.png'
 import weatherIcon from '../assets/weather icon.svg'
 import notFound from '../assets/not-found.png'
+import clear from '../assets/clouds/clear.png'
+import cloud from '../assets/clouds/cloud.png'
+import brokenCloud from '../assets/clouds/broken-cloud.png'
+import haze from '../assets/clouds/haze.png'
+import lightRain from '../assets/clouds/light-rain.png'
+import overcast from '../assets/clouds/overcast.png'
+import thunderStorm from '../assets/clouds/thunder-storm.png'
 
 function Weather() {
   const [city, setCity] = useState("")
+  const [lon, setLon] = useState("")
+  const [lat, setLat] = useState("")
   const [weather, setWeather] = useState(null)
   const [name, setName] = useState("")
   const [tempMin, setTempMin] = useState("")
@@ -26,16 +30,47 @@ function Weather() {
   const [wind, setWind] = useState("")
   const [condition, setCondition] = useState("")
   const [icon, setIcon] = useState("")
+  const [suggestion, setSuggestion] = useState([])
 
   const [loading, setLoading] = useState(false)
+  const [geoLoad, setGeoLoad] = useState(true)
   const [error, setError] = useState(false)
   const [networkError, setNetworkError] = useState(false)
 
   const API_KEY = "d0c5cdce71ec8d1203cff6fd6c267853"
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat=${lon}&lon=${lat}&appid=${API_KEY}&units=metric`
+  
+  const fetchSuggestion = async (value) => {
+    const suggestion_API = `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=100&appid=${API_KEY}`
+    try{
+      const response = await fetch(suggestion_API)
+      const data = await response.json()
+
+      if(response.ok){
+        setSuggestion([...data])
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleClick = (item) => {
+    const cityName = `${item.name}, ${item.country}`
+    const logitute = item.lon
+    const latitude = item.lat
+    setCity(cityName)
+    setLon(logitute)
+    setLat(latitude)
+    setSuggestion([])
+    fetchWeather()
+  }
+
+  const suggestions = suggestion.map((item, index) => (<li key={index} onClick={() => handleClick(item)} className="py-2 px-4 font-bold hover:bg-gray-500 cursor-pointer">{item.name} {item.country}</li>))
 
   const handleCity = (e) => {
-    setCity(e.target.value)
+    const value = e.target.value
+    setCity(value)
+    fetchSuggestion(value)
   }
 
   const fetchWeather = async () => {
@@ -79,26 +114,40 @@ function Weather() {
       setFeelsLike(`${(weather.main.feels_like).toFixed()}°C`)
       setWind(`${weather.wind.speed}Km/h`)
       setCondition(weather.weather[0].description)
+
+      if(condition.toLowerCase() === "overcast clouds"){
+        setIcon(overcast)
+      }else if(condition.toLowerCase() === "broken clouds" || condition.toLowerCase() === "few clouds" || condition.toLowerCase() === "scattered clouds"){
+        setIcon(brokenCloud)
+      }else if(condition.toLowerCase() === "thunderstorm"){
+        setIcon(thunderStorm)
+      }else if(condition.toLowerCase() === "haze"){
+        setIcon(haze)
+      }else if(condition.toLowerCase() === "light rain"){
+        setIcon(lightRain)
+      }else if(condition.toLowerCase() === "cloud"){
+        setIcon(cloud)
+      }else{
+        setIcon(clear)
+      }
     }
-    
-    if(condition.toLowerCase() === "clear sky"){
-      setIcon(clearSky)
-    }else if(condition.toLowerCase() === "broken clouds"){
-      setIcon(broken)
-    }else if(condition.toLowerCase() === "overcast clouds" || condition.toLowerCase() === "thunderstorm" || condition.toLowerCase() === "light rain"){
-      setIcon(overcast)
-    }else if(condition.toLowerCase() === "haze"){
-      setIcon(haze)
-    }else{
-      setIcon(clouds)
-    } 
+
   }, [weather, condition])
 
   return(
     <div className="max-w-85  h-100 rounded-2xl mx-auto p-3 backdrop-blur-sm bg-white/5">
-      <div className="w-100% flex bg-white/3 justify-between rounded-full py-2 px-5">
+      <div className="w-100% flex bg-white/3 justify-between rounded-full py-2 px-5 relative">
         <input type="text" value={city} onChange={(e) => handleCity(e)} onKeyDown={(e) => handleOnKeyDown(e)} placeholder="Search City" className="w-4/5 outline-none text-white"/>
         <button onClick={fetchWeather}><img src={search} alt="" className="w-7 invert cursor-pointer"/></button>
+        {
+          suggestion.length !== 0 && city.trim() !== "" && (
+            <div className="absolute bg-white/80 backdrop-blur-lg top-[100%] w-70 rounded z-1">
+            <ul className="flex flex-col ">
+              {suggestions}
+            </ul>
+        </div>
+          )
+        }
       </div>
       {loading ? (<SkeletonLoader />) : weather ? (
       <div className="px-3">
